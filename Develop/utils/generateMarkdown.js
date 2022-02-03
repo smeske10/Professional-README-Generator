3a34fd845905181cfcaabcb557f9b9b64b273665
smeske10 <35307525+smeske10@users.noreply.github.com>
2022-02-03 17:17:19 -0500

    Created functions that pass data from Inquirer to generate a README

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,79 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicense(license) {
+  if (license ==='Apache') {
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  }
+  if (license ==='Unlicense') {
+    return ''
+  }
+  if (license ==='MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }
+  if (license ==='Berkeley Source Distribution (BSD)') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }
+  if (license ==='GNU General Public License (GPL)') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  }
+  if (license ==='Mozilla Public License (MPL)') {
+    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+  }
+  if (license ==='Eclipse Public License (EPL)') {
+    return '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)'
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+## Description
+- ${data.why}
+- ${data.problem}
+- ${data.learn}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+To install this application, please follow the steps below:
+- ${data.install}
+
+## Usage
+
+![alt text](assets/images/screenshot.png)
+
+## Features
+
+- ${data.features}
+
+## Contributors & Credit
+
+- ${data.contributors}
+- ${data.thirdparty}
+
+## Tests
+
+- ${data.test}
+
+## Questions 
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
+
+## License
+
+- ${renderLicense(data.license)}
+
+Refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+## How to Contribute
+
+- ${data.contribute}
+The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
 
 `;
 }
